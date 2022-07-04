da011b0bd1454881e7bf6e22c0177e3e13a12373
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-07-04 23:11:07 +0300

    add HW 2 Sum From M To N

--- HW/01_MultiMToN/Program.cs
+++ HW/01_MultiMToN/Program.cs
@@ -5,17 +5,12 @@
 using System;
 using static System.Console;
 
+Clear();
 Write("Введите M: ");
 int m = Convert.ToInt32(ReadLine());
 Write("Введите N: ");
 int n = Convert.ToInt32(ReadLine());
 WriteLine($"M = {m}; N = {n} -> {MultiFromMToN(m, n)}");
-
-// int MultiFromMToN (int m, int n) {
-// 	if (n == m)		return n;
-// 	else if (n > m)	return n * MultiFromMToN(m, n - 1);
-// 	else 			return m * MultiFromMToN(m - 1, n);
-// }
 
 int MultiFromMToN (int m, int n) {
 	return n == m ? n : n > m ? n * MultiFromMToN(m, n - 1) : m * MultiFromMToN(m - 1, n);
---
+++ HW/02_SumMToN/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите M: ");
+int m = Convert.ToInt32(ReadLine());
+Write("Введите N: ");
+int n = Convert.ToInt32(ReadLine());
+WriteLine($"M = {m}; N = {n} -> {SumFromMToN(m, n)}");
+
+int SumFromMToN (int m, int n) {
+	return n == m ? n : n > m ? n + SumFromMToN(m, n - 1) : m + SumFromMToN(m - 1, n);
+}
